f91c6a037e5fd0bcea13305d02392dc0943c330e
Guilherme Vieira de Moraes <guilhermevdm@gmail.com>
2020-06-26 10:28:43 -0300

    create instance of the store and typedHooks for better developer experience

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import store from './store';
+import { StoreProvider } from 'easy-peasy';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <StoreProvider store={store}>
+      <CssBaseline />
+      <App />
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/store/index.ts
@@ -0,0 +1,3 @@
+export { default } from './store';
+export { default as store } from './store';
+export * from './hooks';
